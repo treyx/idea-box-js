93f8767c65e14436a4767eef12167a19332397cf
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-05-20 13:29:31 -0600

    added routes and controller actions for create and update

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -7,4 +7,18 @@ class Api::V1::IdeasController < ApplicationController
   def show
     respond_with Idea.find_by(id: params[:id])
   end
+
+  def create
+    respond_with :api, :v1, Idea.create(idea_params)
+  end
+
+  def update
+    respond_with :api, :v1, Idea.update(params[:id], idea_params)
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:title, :body)
+  end
 end
