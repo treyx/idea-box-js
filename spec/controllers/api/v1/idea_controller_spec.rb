a55ad7f1855bbb4476dae8a0a0e49aa59bd4fc50
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-05-20 13:29:03 -0600

    specs for create and update

--- spec/controllers/api/v1/idea_controller_spec.rb
+++ spec/controllers/api/v1/idea_controller_spec.rb
@@ -22,4 +22,28 @@ RSpec.describe Api::V1::IdeasController, type: :controller do
     expect(first_idea[:title]).to eq(idea.title)
     expect(first_idea[:body]).to eq(idea.body)
   end
+
+
+  it "responds to #create" do
+    idea = Idea.new(title: "title", body: "body")
+    expect do
+      post :create, format: :json, idea: { title: idea.title, body: idea.body }
+
+      idea = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(201)
+
+      expect(idea[:title]).to eq("title")
+      expect(idea[:body]).to eq("body")
+    end.to change{ Idea.count }.from(0).to(1)
+  end
+
+  it "responds to #update" do
+    old_idea = Idea.create(title: "title", body: "body")
+    expect(Idea.first.title).to eq(old_idea.title)
+    put :update, format: :json, id: old_idea.id, idea: { title: "new title", body: "new body"}
+    expect(Idea.first.title).to eq("new title")
+    expect(Idea.first.body).to eq("new body")
+    expect(response.status).to eq(204)
+  end
 end
